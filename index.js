278166e5d4b3a8ea3abd9a253a3470e4e91b14d6
matzabreu <abreumagalhaes@outlook.com>
2023-12-03 22:03:48 -0300

    exclusão de tarefas e finalização do projeto

--- index.js
+++ index.js
@@ -13,67 +13,26 @@ app.use(express.urlencoded({
     extended: true
 }))
 
-//rotas
+app.use(express.json())
+
+app.get("/limpartarefas", (requisicao, resposta) => {
+    const sql = 'DELETE FROM tarefas'
+
+    conexao.query(sql, (erro) => {
+        if(erro) {
+            console.log(erro)
+        }
+
+        resposta.redirect("/")
+    })
+})
+
 app.post('/excluir', (requisicao, resposta) => {
     const id = requisicao.body.id
 
     const sql = `
         DELETE FROM tarefas
         WHERE id = ${id}
-    `
-
-    conexao.query(sql, (erro) => {
-        if (erro) {
-            return console.log(erro)
-        }
-
-        resposta.redirect('/')
-    })
-})
-
-app.post('/completar', (requisicao, resposta) => {
-    const id = requisicao.body.id
-
-    const sql = `
-        UPDATE tarefas
-        SET completa = '1'
-        WHERE id = ${id}
-    `
-
-    conexao.query(sql, (erro) => {
-        if (erro) {
-            return console.log(erro)
-        }
-
-        resposta.redirect('/')
-    })
-})
-
-app.post('/descompletar', (requisicao, resposta) => {
-    const id = requisicao.body.id
-
-    const sql = `
-        UPDATE tarefas
-        SET completa = '0'
-        WHERE id = ${id}
-    `
-
-    conexao.query(sql, (erro) => {
-        if (erro) {
-            return console.log(erro)
-        }
-
-        resposta.redirect('/')
-    })
-})
-
-app.post('/criar', (requisicao, resposta) => {
-    const descricao = requisicao.body.descricao
-    const completa = 0
-
-    const sql = `
-        INSERT INTO tarefas(descricao, completa)
-        VALUES ('${descricao}', '${completa}')
     `
 
     conexao.query(sql, (erro) => {
@@ -147,17 +106,71 @@ app.get('/', (requisicao, resposta) => {
             return {
                 id: dado.id,
                 descricao: dado.descricao,
-                completa: dado.completa === 0 ? false : true
-            }
+                completa: dado.completa === 0 ? false : true 
+           }
         })
 
-        const tarefasAtivas = tarefas.filter((tatefa) => {
-            return tarefas.completa === false && tarefa
+        const tarefasAtivas = tarefas.filter((tarefa) => {
+            return tarefa.completa === false && tarefa
         })
 
         const quantidadeTarefasAtivas = tarefasAtivas.length
 
         resposta.render('home', { tarefas, quantidadeTarefasAtivas })
+    })
+})
+
+app.post('/completar', (requisicao, resposta) => {
+    const id = requisicao.body.id
+
+    const sql = `
+        UPDATE tarefas
+        SET completa = '1'
+        WHERE id = ${id}
+    `
+
+    conexao.query(sql, (erro) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+})
+
+app.post('/descompletar', (requisicao, resposta) => {
+    const id = requisicao.body.id
+
+    const sql = `
+        UPDATE tarefas
+        SET completa = '0'
+        WHERE id = ${id}
+    `
+
+    conexao.query(sql, (erro) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+})
+
+app.post('/criar', (requisicao, resposta) => {
+    const descricao = requisicao.body.descricao
+    const completa = 0
+
+    const sql = `
+        INSERT INTO tarefas(descricao, completa)
+        VALUES ('${descricao}', '${completa}')
+    `
+
+    conexao.query(sql, (erro) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
     })
 })
 
@@ -181,116 +194,3 @@ conexao.connect((erro) => {
         console.log("Servidor rodando na porta 3000!")
     })
 })
-
-// const express = require("express")
-// const exphbs = require("express-handlebars")
-// const mysql = require("mysql2")
-
-// const app = express()
-
-// app.engine('handlebars', exphbs.engine())
-// app.set('view engine', 'handlebars')
-
-// app.use(express.static('public'))
-
-// app.use(express.static('public'))
-
-// // converter dados do formulario em objeto javascript
-// app.use(express.urlencoded({
-//     extended: true
-// }))
-
-// //rotas
-// // app.post('/completar', (requisicao, resposta) => {
-// //     const id = requisicao.body.id
-// //     console.log(requisicao.body)
-// //     const sql = `
-// //         UPDATE tarefas
-// //         SET completa = '1'
-// //         WHERE id = ${id}
-// //     `
-
-// //     conexao.query(sql, (erro) => {
-// //         if (erro) {
-// //             return console.log(erro)
-// //         }
-
-// //         resposta.redirect('/')
-// //     })
-// // })
-
-// app.post('/completar', (requisicao, resposta) => {
-//     const id = requisicao.body.id
-
-//     const sql = `
-//         UPDATE tarefas
-//         SET completa = '1'
-//         WHERE id = ${id}
-//     `
-
-//     conexao.query(sql, (erro) => {
-//         if (erro) {
-//             return console.log(erro)
-//         }
-
-//         resposta.redirect('/')
-//     })
-// })
-
-// app.post('/criar', (requisicao, resposta) => {
-//     const descricao = requisicao.body.descricao
-//     const completa = 0
-
-//     const sql = `
-//         INSERT INTO tarefas(descricao, completa)
-//         VALUES ('${descricao}', '${completa}')
-//     `
-
-//     conexao.query(sql, (erro) => {
-//         if(erro) {
-//             return console.log(erro)
-//         }
-
-//         resposta.redirect('/')
-//     })
-// })
-
-// app.get('/', (requisicao, resposta) => {
-//      const sql = 'SELECT * FROM tarefas'
-
-//      conexao.query(sql, (erro, dados) => {
-//          if (erro) {
-//              return console.log(erro)
-//          }
-
-//          const tarefas = dados.map((dado) => {
-//              return {
-//                  id: dado.id,
-//                  descricao: dado.descricao,
-//                  completa: dado.completa === 0 ? false : true
-//              }
-//          })
-
-//          resposta.render('home', { tarefas })
-//      })
-//  })
-
-// const conexao = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "54669009",
-//     database: "todoapp",
-//     port: 3306
-// })
-
-// conexao.connect((erro) => {
-//     if (erro) {
-//         return console.log(erro)
-//     }
-
-//     console.log("Estou conectado ao MySQL.")
-
-//     app.listen(3000, () => {
-//         console.log("Servidor rodando na porta 3000!")
-//     })
-// })
--- public/js/script.js
+++ public/js/script.js
@@ -10,6 +10,16 @@ function completarTarefa(id) {
     window.location.reload()
 }
 
+function excluirTarefa(id) {
+    fetch("http://localhost:3000/excluir", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ id })
+    })
+}
+
 function descompletarTarefa(id) {
     fetch("http://localhost:3000/descompletar", {
         method: "POST",
@@ -20,16 +30,6 @@ function descompletarTarefa(id) {
     })
 
     window.location.reload()
-}
-
-function excluirTarefa(id) {
-    fetch("http://localhost:3000/excluir", {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ id })
-    })
 }
 
 function alterarTema() {
@@ -77,61 +77,3 @@ function verificarTema() {
 }
 
 verificarTema()
-
-// function completarTarefa(id) {
-//     fetch("http://localhost:3000/completar", {
-//         method: "POST",
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({ id })
-//     })
-
-//     window.location.reload()
-// }
-
-// function alterarTema() {
-//     const tema = localStorage.getItem("tema")
-//     const body = document.querySelector("body")
-//     const button = document.querySelector(".tema-button")
-
-//     if (tema) {
-//         let novoTema
-
-//         if (tema === "light") {
-//             novoTema = "dark"
-//             button.innerHTML = `<img src="/imagens/sun-icon.png" alt="icone do sol">`
-//             body.classList.remove("light")
-//             body.classList.add("dark")
-//         } else {
-//             novoTema = "light"
-//             button.innerHTML = `<img src="/imagens/moon-icon.png" alt="icone de lua">`
-//             body.classList.remove("dark")
-//             body.classList.add("light")
-//         }
-
-//         localStorage.setItem("tema", novoTema)
-//         return
-//     }
-
-//     localStorage.setItem("tema", "dark")
-//     body.classList.add("dark")
-// }
-
-// function verificarTema() {
-//     const tema = localStorage.getItem("tema")
-//     const body = document.querySelector("body")
-//     const button = document.querySelector(".tema-button")
-
-//     if (tema) {
-//         if (tema === "dark") {
-//             body.classList.add("dark")
-//             button.innerHTML = `<img src="/imagens/sun-icon.png" alt="icone do sol">`
-//         } else {
-//             body.classList.add("light")
-//             button.innerHTML = `<img src="/imagens/moon-icon.png" alt="icone de lua">`
-//         }
-//     }
-// }
-
-// verificarTema()
